7bb6a757bb7b03754702b29b9c10299d9be8082d
Manoj Dayaram <noj@moovweb.com>
2014-12-26 23:52:46 -0800

    adding setter to tweet class

--- models/tweet.rb
+++ models/tweet.rb
@@ -3,6 +3,10 @@ require 'sinatra/activerecord'
 class Tweet < ActiveRecord::Base
   @@client = nil # initialized in environment.
 
+  def self.client=(client)
+    @@client = client
+  end
+
   def after_save
 
   end
